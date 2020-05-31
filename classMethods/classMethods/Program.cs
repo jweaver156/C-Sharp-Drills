4662e6a9afd67abbcf9c84cd2110871e5d5e8ab2
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-05-31 12:33:11 -0700

    Add files via upload

---
+++ classMethods/classMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            Console.Write("Enter a value: ");
+            int.TryParse(Console.ReadLine(),out value);
+
+            //Instance of Tester Class
+            Tester tester = new Tester(value);
+
+            //Call First Method
+            Console.WriteLine("The double value of {0} is {1}", value, tester.multiplyBy2());
+
+            //Call Second Method
+            Console.WriteLine("The sqaure value of {0} is {1}", value, tester.getSquare());
+
+            //Call Third Method
+            Console.WriteLine("The square root value of {0} is {1}", value, tester.getSquareRoot());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ classMethods/classMethods/Tester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethods
+{
+    public class Tester
+    {
+        public int value;
+        public Tester(int value)
+        {
+            this.value = value;
+        }
+
+        //First Method Multiply Value By 2
+        public int multiplyBy2()
+        {
+            int doubleValue = 2 * value;
+            return doubleValue;
+        }
+
+        //Second Method Returns the Square Root of Value
+        public int getSquare()
+        {
+            int square = value * value;
+            return square;
+        }
+
+        //Third Method Returns Square Root of Value
+        public double getSquareRoot()
+        {
+            double root = Math.Sqrt(value);
+            return root;
+        }
+    }
+}
