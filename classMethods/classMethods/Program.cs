d66b1317c5a28716e7e5eaa059ec925fb2df9117
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-05-31 15:53:04 -0700

    Add files via upload

--- classMethods/classMethods/Program.cs
+++ classMethods/classMethods/Program.cs
@@ -10,21 +10,22 @@ namespace classMethodsParameters
     {
         static void Main(string[] args)
         {
-            int value = 0;
-            Console.Write("Enter a value: ");
+            int value;
+    
+            Console.Write("Enter a number you would like to multiply by 2,\nget the square value of,\nand find the square root of: ");
             int.TryParse(Console.ReadLine(),out value);
 
             //Instance of Tester Class
             Tester tester = new Tester();
 
             //Call First Method
-            Console.WriteLine("The double value of {0} is {1}", value, tester.multiplyBy2(value));
+            Console.WriteLine("{0} multiplied by 2 is {1},", value, tester.multiplyBy2(value));
 
             //Call Second Method
-            Console.WriteLine("The square value of {0} is {1}", value, tester.getSquare(value));
+            Console.WriteLine("{0} squared is {1},", value, tester.getSquare(value));
 
             //Call Third Method
-            Console.WriteLine("The square root value of {0} is {1}", value, tester.getSquareRoot(value));
+            Console.WriteLine("and the square root of {0} is {1}.", value, tester.getSquareRoot(value));
 
             Console.ReadLine();
         }
