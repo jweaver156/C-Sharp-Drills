6a764aabd02bd26eac0d81f3af9d13bf67731835
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-05-31 15:38:43 -0700

    Add files via upload

--- classMethods/classMethods/Program.cs
+++ classMethods/classMethods/Program.cs
@@ -4,27 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace classMethods
+namespace classMethodsParameters
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int value;
+            int value = 0;
             Console.Write("Enter a value: ");
             int.TryParse(Console.ReadLine(),out value);
 
             //Instance of Tester Class
-            Tester tester = new Tester(value);
+            Tester tester = new Tester();
 
             //Call First Method
-            Console.WriteLine("The double value of {0} is {1}", value, tester.multiplyBy2());
+            Console.WriteLine("The double value of {0} is {1}", value, tester.multiplyBy2(value));
 
             //Call Second Method
-            Console.WriteLine("The sqaure value of {0} is {1}", value, tester.getSquare());
+            Console.WriteLine("The square value of {0} is {1}", value, tester.getSquare(value));
 
             //Call Third Method
-            Console.WriteLine("The square root value of {0} is {1}", value, tester.getSquareRoot());
+            Console.WriteLine("The square root value of {0} is {1}", value, tester.getSquareRoot(value));
 
             Console.ReadLine();
         }
