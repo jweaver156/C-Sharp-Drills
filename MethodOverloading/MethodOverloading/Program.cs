d7c073e96402b80e75e4e5a338e09ec69d5aed21
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-02 13:32:31 -0700

    Add files via upload

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instatnsting object for class Solutions
+            Solutions solutions = new Solutions();
+
+            //Call function for int.
+            int res = solutions.Eval(4);
+            Console.WriteLine(res);
+
+            //Call function for double into int.
+            res = solutions.Eval(7.25);
+            Console.WriteLine(res);
+
+            //Call function for string into int.
+            res = solutions.Eval("20");
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Solutions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Solutions
+{
+
+
+    //Method takes in an int and multiplies by 2.
+    public int Eval(int a)
+    {
+        return a * 2;
+    }
+
+    //Method takes in a decimal and adds 10.
+    public int Eval(double a)
+    {
+        return (int)a + 10;
+    }
+
+    //Method converts string to int and subtracts by 2.
+    public int Eval(string a)
+    {
+
+        int num = Convert.ToInt32(a);
+        num = num - 2;
+        return num;       
+       
+    }
+}
