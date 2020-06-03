d26f2fe83f5571d0ce2f063b3ad5456148d282d8
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-03 11:37:08 -0700

    Add files via upload

---
+++ VoidMethod/VoidMethod/MathOper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class MultiBy
+{
+    //Defining class vMethod
+    public void vMethod(int a, int b)
+    {
+        //Multiply first interger by 2
+        a = a * 2;
+
+        //Prints second integer
+        Console.WriteLine("Second integer is: {0}", b);
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //Instantiating MultiBy class
+        MultiBy multiBy = new MultiBy();
+
+        //Calling method vMethod by parameter names
+        multiBy.vMethod(a: 3, b: 4);
+
+        
+    }
+}
