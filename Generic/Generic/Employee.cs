5d4841b32317fe2b0a19ee018e21911e1853d7ac
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-08 22:13:32 -0700

    Add files via upload

---
+++ Generic/Generic/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Generic
+{
+    public class Employee<T>
+    {
+        public T[] things;
+
+        public Employee(T[] items)
+        {
+            things = items;
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < things.Length; i++)
+            {
+                Console.Write(things[i] + " ");                
+            }               
+        }
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ListString = { "Using", "generics", "have", "it\'s", "advantages." };
+
+            Employee<string> EmployStr  = new Employee<string>(ListString);
+            EmployStr.print();
+
+            int[] ListInt = { 10, 20, 30, 40, 50 };
+
+            Employee<int> EmployInt = new Employee<int>(ListInt);
+            EmployInt.print();
+        }
+    }
+}
