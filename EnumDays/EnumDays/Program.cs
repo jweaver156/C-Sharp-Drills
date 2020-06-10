a666b5df7ea6fa60cd7493870b438d5c8c320f60
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-09 19:10:01 -0700

    Add files via upload

---
+++ EnumDays/EnumDays/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class EDays
+{
+    enum Days
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    public static void Main()
+    {
+
+        try
+        {
+            Console.WriteLine("What day is it today?:");           
+            string day = Console.ReadLine();
+            day = char.ToUpper(day[0]) + day.Substring(1);
+
+            Days ValidInput;
+            switch (day)
+            {
+                case "Sunday":
+                    ValidInput = Days.Sunday;
+                    break;
+
+                case "Monday":
+                    ValidInput = Days.Monday;
+                    break;
+
+                case "Tuesday":
+                    ValidInput = Days.Tuesday;
+                    break;
+
+                case "Wednesday":
+                    ValidInput = Days.Wednesday;
+                    break;
+
+                case "Thursday":
+                    ValidInput = Days.Thursday;
+                    break;
+
+                case "Friday":
+                    ValidInput = Days.Friday;
+                    break;
+
+                case "Saturday":
+                    ValidInput = Days.Saturday;
+                    break;
+
+                default:
+                    throw new Exception("Enter the actual day of the week.");
+            }
+
+            Console.WriteLine("Today is " + ValidInput.ToString() + ".");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            
+        }
+    }
+}
