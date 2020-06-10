cd31fa209a57d7926ddb26179408d22e2a6659b5
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-09 20:11:46 -0700

    Add files via upload

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Structs
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 1.0m;
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
