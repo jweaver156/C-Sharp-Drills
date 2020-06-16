377f61e17fc53b215c6a2dd35cd19c8b64f8023e
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-16 16:13:54 -0700

    Add files via upload

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace DateTime1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a single digit number");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine(DateTime.Now.AddHours(hours));
+
+        }
+    }
+}
