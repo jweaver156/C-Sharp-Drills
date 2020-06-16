e9cd1dc651bfccad1090c22e95643ac25d02c4e2
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-16 14:49:47 -0700

    Add files via upload

---
+++ FileOI/FileOI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FileOI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string userNum = Console.ReadLine();
+
+            File.WriteAllText(@"Users\John\Documents\log\log.txt", userNum);
+
+            Console.WriteLine(File.ReadAllText(@"Users\John\Documents\log\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
