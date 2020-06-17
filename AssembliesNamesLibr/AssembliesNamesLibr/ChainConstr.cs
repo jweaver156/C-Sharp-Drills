556b89ffad28ff0c7794dd91c6afa0efa8dfbcc0
John Weaver <46185473+jweaver156@users.noreply.github.com>
2020-06-16 19:30:14 -0700

    Add files via upload

---
+++ AssembliesNamesLibr/AssembliesNamesLibr/ChainConstr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AssembliesNamesLibr
+{
+    public class ChainConstr
+    {
+        public string favBev;
+        public ChainConstr() : this("Coffee")
+        {
+            
+        }
+
+        public ChainConstr(string bestBev)
+        {
+            favBev = bestBev;
+        }
+    }
+}
---
+++ AssembliesNamesLibr/AssembliesNamesLibr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssembliesNamesLibr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string doesNotChange = "I am a constant variable";
+            Console.WriteLine("{0} and will never change.", doesNotChange);
+
+            var num = 7;
+            Console.WriteLine(num);
+
+            ChainConstr chainConstr = new ChainConstr();
+            Console.WriteLine(chainConstr.favBev);
+        }
+    }
+}
